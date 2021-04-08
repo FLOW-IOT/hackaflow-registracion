ae9d92530ac37e623a542182aeae7868362960d0
Jesus Africano <JJAfricanoUrbina@teco.com.ar>
2021-04-08 14:28:27 -0300

    avances
    Co-authored-by: Rodrigo Vila <rodrigo.s.vila@gmail.com>

---
+++ src/components/QRCode.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { QRImage } from '../assets/QRCode.png';
+
+const QRCode = () => {
+  return (
+    <div>
+      <img src={QRImage} />
+    </div>
+  );
+};
+
+export default QRCode;
