fd4b46499b76375b7d54434b9e66fe94993e7734
Rodrigo Vila <rodrigo.s.vila@gmail.com>
2021-04-09 13:42:12 -0300

    Opacity transition added

--- src/screens/STVSuccess.js
+++ src/screens/STVSuccess.js
@@ -1,8 +1,23 @@
+import { useState, useEffect } from "react";
 import profilesImg from "../assets/profiles.png";
 
 const STVSuccess = () => {
+  const [opacity, setOpacity] = useState("opacity-1");
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setOpacity("opacity-0");
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div className="h-screen bg-black text-white px-20">
+    <div className="h-screen bg-black text-white px-20 flex items-center justify-center">
+      <div
+        className={`absolute top-10 left-10 text-2xl transition transition-opacity duration-1000 ${opacity}`}
+      >
+        Te logueaste con exito.
+      </div>
       <img src={profilesImg} alt="Imagen de Perfiles" />
     </div>
   );
