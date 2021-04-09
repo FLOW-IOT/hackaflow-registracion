fb649d64bb8fc34a12dc84f6c9a04eacb99cf132
LucianABC <louabecasis@gmail.com>
2021-04-09 11:01:00 -0300

    Deleted email and password inputs, some styling

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,28 +1,43 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
 const InputWrapper = styled.div`
+  && input {
+    border: ${(props) => (props.error ? "2px solid red" : "none")};
+    font-family: "roboto", sans-serif;
+    border-bottom: 2px solid #686868;
+    color: #686868;
+    background-color: #000;
+    &::after {
+      border-bottom: 2px solid #686868;
+      color: #686868;
+      background-color: #000;
+    }
+  }
+`;
 
-&& input{
-    border: ${props => props.error ? '2px solid red' : 'none'};
-    color: black;
-}
-`
-
-const Input = ({name ,type, value, changeCallback, blurCallback, placeholder, error}) => {
-    return (
-        <InputWrapper
-        error={error}>
+const Input = ({
+  name,
+  type,
+  value,
+  changeCallback,
+  blurCallback,
+  placeholder,
+  error,
+}) => {
+  return (
+    <InputWrapper error={error}>
       <input
-      name={name}
-      type={type}
-      onChange={(e) => changeCallback(e)}
-      onBlur={e => blurCallback(e)}
-      value={value}
-        />
-        {error && <p>{error}</p>}
+        name={name}
+        type={type}
+        onChange={(e) => changeCallback(e)}
+        onBlur={(e) => blurCallback(e)}
+        value={value}
+        placeholder={placeholder}
+      />
+      {error && <p>{error}</p>}
     </InputWrapper>
-    )
-}
+  );
+};
 
-export default Input
+export default Input;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,104 +1,91 @@
 import React, { useState } from "react";
-import styled from 'styled-components'
-import Input from './Input'
+import styled from "styled-components";
+import Input from "./Input";
 
 const Wrapper = styled.div`
   display: flex;
   padding-top: 50px;
-  & div:nth-child(1){
-    max-width:25%;
+  & div:nth-child(1) {
+    max-width: 25%;
   }
-`
+`;
 const Container = styled.div`
-color: white;
+  color: white;
   display: flex;
-  flex-direction:column;
-`
+  flex-direction: column;
+`;
 const LoginButton = styled.button`
   text-transform: uppercase;
   color: white;
   padding: 8px;
   padding-left: 15px;
   border: none;
-  border-left: 5px solid #57D4AC;
+  border-left: 5px solid #57d4ac;
   font-size: 36px;
   background: none;
-  font-family: 'roboto', sans-serif;
+  font-family: "roboto", sans-serif;
   margin: 15px;
-`
+  font-weight: bold;
+`;
+
+const Title = styled.p`
+  font-family: "roboto", sans-serif;
+  font-size: 36px;
+`;
+
+const SubmitButton = styled.button`
+  background-color: #57d4ac;
+  border-radius: 20px;
+  padding: 10px;
+  width: 250px;
+  color: #000;
+  ::placeholder {
+    font-family: "roboto", sans-serif;
+    font-size: 36px;
+  }
+`;
+
 const Login = () => {
-  const [email, setEmail] = useState({value:"", error: null});
-  const [password, setPassword] = useState({value:"", error: null});
-  const [code, setCode] = useState();
+  const [code, setCode] = useState("");
+  const [error, setError] = useState("");
 
   const handleSubmit = () => {
     //mandamo toda la data al back asi re lindo
   };
-  const handleChange = ({target}) =>{
-    const {name, value} = target;
-    if(name === 'email'){
-        setEmail({value, error: null})
+
+  const handleChange = ({ target }) => {
+    const { value } = target;
+    setCode(value);
+  };
+
+  const handleError = () => {
+    if (!code) {
+      setError("No puede estar en blanco");
+    } else {
+      setError("");
     }
-    if(name === 'password'){
-        setPassword({value, error: null})
-    }
-  }
-  const handleError = ({target}) =>{
-    const {name, value} = target;
-    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if(name === 'email'){
-      if(!email.value){
-        setEmail({...email, error: 'email cannot be blank'})
-      }
-      if(!re.test(String(email.value).toLowerCase())){
-        setEmail({...email, error: 'Please use a vald email'})
-      }
-    }
-    if(name === 'password'){
-      if(!password.value){
-        setPassword({...password, error: 'password cannot be empty'})
-      }
-      else if(password.value.length < 6){
-        setPassword({...password, error: 'password too short'})
-      }
-    }
-  }
+  };
+
   return (
     <Wrapper>
       <Container>
-        <LoginButton>login</LoginButton>
+        <LoginButton>easy login</LoginButton>
       </Container>
       <Container>
-        <Input
-          name="email"
-          type="email"
-          value={email.value}
-          placeholder="email"
-          changeCallback={handleChange}
-          blurCallback={handleError}
-          error={email.error}
-          />
-          <Input
-          name="password"
-          type="password"
-          value={password.value}
-          placeholder="password"
-          changeCallback={handleChange}
-          blurCallback={handleError}
-          error={password.error}
-          />
+        <Title>
+          ¿Querés loguearte en tu TV? Ingresa que el código que aparece en la TV
+        </Title>
 
-      <input
-        type="password"
-        onChange={(e) => setPassword(e.target.value)}
-        value={password}
-      />
-      <input
-        type="code"
-        onChange={(e) => setCode(e.target.value)}
-        value={code}
-      />
-      <button onClick={handleSubmit}> ENTRAR CON TODA VIOLENCIA</button>
+        <Input
+          name="code"
+          type="text"
+          value={code}
+          placeholder="Código"
+          changeCallback={handleChange}
+          blurCallback={handleError}
+          error={error}
+        />
+        <SubmitButton onClick={handleSubmit}>INGRESAR</SubmitButton>
       </Container>
     </Wrapper>
   );
