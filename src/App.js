c2750be72716480ed54c762413b4792c82bcfbbd
Rodrigo Vila <rodrigo.s.vila@gmail.com>
2021-04-09 14:48:50 -0300

    Success Route added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {
 
 import PortalScreen from "./screens/PortalScreen";
 import STVScreen from "./screens/STVScreen";
+import STVSuccess from "./screens/STVSuccess";
 
 function App() {
   return (
@@ -18,6 +19,9 @@ function App() {
           </Route>
           <Route path="/stv">
             <STVScreen />
+          </Route>
+          <Route path="/success">
+            <STVSuccess />
           </Route>
           <Route exact path="/">
             <Redirect to="/stv" />
